ea1493703e88e81d6ad4c4e9c87ad5801ae28d9a
unknown <paulo.diegooo@gmail.com>
2021-03-31 02:45:47 -0300

    Criação da classe agente
    Co-Authored-By: Thiego <50022720+Dioliver@users.noreply.github.com>

--- ambiente_monstrinho.py
+++ ambiente_monstrinho.py
@@ -1,93 +1,131 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[13]:
-import random 
+import random
 
-def imprime(l, matriz):
-    c = l
-    for i in range(l):
-        for j in range(c):
-            if(j == c - 1):
-                print("[%s],\n" %matriz[i][j], end = "")
-            else:
-                print("[%s]" %matriz[i][j], end = "")
-                print(" ", end = "")
+list_pos = [] #Lista vazia de sensações
+
+class Ambiente:
     
-def crie_matriz(n_linhas):
-    n_colunas = n_linhas  
-    matriz = [] # lista vazia
-    for i in range(n_linhas):
-        # cria a linha i
-        linha = [] # lista vazia
-        for j in range(n_colunas):
-            linha += ['.']
-        # coloque linha na matriz
-        matriz += [linha]
-    return matriz
+    def __init__ (self, tam):
+        self.tam = tam
 
-def num_rand(t):
-    return random.randrange(0, t)
+    def imprime(self, matriz):
+        l = self.tam
+        c = self.tam
+        for i in range(l):
+            for j in range(c):
+                if(j == c - 1):
+                    print("[%s],\n" % matriz[i][j], end="")
+                else:
+                    print("[%s]" % matriz[i][j], end="")
+                    print(" ", end="")
 
-def pos_elem(A, ob):
-    while 1:
-        n1 = num_rand(len(A))
-        n2 = num_rand(len(A))
-        if A[n1][n2] == '.':
-            A[n1][n2] = ob
-            break
+
+    def crie_matriz(self):
+        l = self.tam
+        c = self.tam
+        matriz = []  # lista vazia
+        for i in range(l):
+            # cria a linha i
+            linha = []  # lista vazia
+            for j in range(c):
+                linha += ['.']
+            # coloque linha na matriz
+            matriz += [linha]
+        return matriz
+
+
+
+    def pos_elem(self, A, ob):  # Evitar que gere poço na casa [0,0] (Diego)
+
+        if ob == 'A':
+            A[0][0] = ob
         else:
-            continue
-    
-def gen_sensacoes(mtz, size):
-    #essa e a funcao 
-    aux = size #numero de colunas e linhas
-    #agora o programa vai procurar em cada linha e em cada coluna 
-    for i in range(size):
-        for j in range(aux):
-            if (mtz[i][j] == 'G'):  #quando o programa achar o ouro (Golden)
-                print(f"ouro em l[{i}] c[{j}]\n", end="")
-                #Em cada coordenada, uma magia pra não pegar posicoes fora do tabuleiro
-                norte = (i, j) if i >= 0 and j >= 0 else None
-                sul = (i-1, j) if i-1 >= 0 and j >= 0 else None
-                oeste = (i, j+1) if i >= 0 and j+1 >= 0 else None
-                leste = (i-1, j)  if i-1 >= 0 and j >= 0 else None
-                #uma lista guarda as posicoes da sensacoes que o wumpus deve ler
-                brilho = [norte, sul, leste, oeste]
-                print("brilho -> ", brilho)
-                print("\n\n")
-            elif (mtz[i][j] == 'H'):  #quando o programa achar o buraco (Hole)
-                print(f"buraco em l[{i}] c[{j}]\n", end="")
-                norte = (i, j) if i >= 0 and j >= 0 else None
-                sul = (i-1, j) if i-1 >= 0 and j >= 0 else None
-                oeste = (i, j+1) if i >= 0 and j+1 >= 0 else None
-                leste = (i-1, j)  if i-1 >= 0 and j >= 0 else None
-                brisa = [norte, sul, leste, oeste]
-                print("brisa -> ", brisa)
-                print("\n\n")
-            elif (mtz[i][j] == 'W'):  #quando o programa achar o wumpus
-                print(f"wumpus em l[{i}] c[{j}]\n", end="")
-                norte = (i, j) if i >= 0 and j >= 0 else None
-                sul = (i-1, j) if i-1 >= 0 and j >= 0 else None
-                oeste = (i, j+1) if i >= 0 and j+1 >= 0 else None 
-                leste = (i-1, j)  if i-1 >= 0 and j >= 0 else None
-                fedor = [norte, sul, leste, oeste]
-                print("fedor -> ", fedor)
-                print("\n\n")
-    
-def main():
-    tam = 9 #definindo o tamanho para a matriz
-    A = crie_matriz(tam) #funcao retorna a matriz
-    pos_elem(A, 'W') #gerando o wumpus
-    pos_elem(A, 'G') #gerando o ouro
-  
-    ###gerando os pocos
-    for i in range(tam-1):
-        pos_elem(A, 'H')
-      
-    imprime(tam,A) #chama a funcao que imprime a matriz
-    gen_sensacoes(A, tam)
-    
-if __name__ == '__main__':
-        main()
+            while 1:
+                n1 = random.randrange(len(A))
+                n2 = random.randrange(len(A))
+                if A[n1][n2] == '.':
+                    A[n1][n2] = ob
+                    break
+                else:
+                    continue
 
+
+    def gen_sensacoes(self, A):  # Tem que arrumar as sensações em Norte (Diego)
+        l = self.tam
+        c = self.tam
+        global list_pos
+        list_bril = []
+        list_brisa = []
+        list_fedor = []
+        # agora o programa vai procurar em cada linha e em cada coluna
+        for i in range(l):
+            for j in range(c):
+                if (A[i][j] == 'G'):  # quando o programa achar o ouro (Golden)
+                    
+                    print(f"ouro em l[{i}] c[{j}]\n", end="")
+                    centro = (i, j)
+                    brilho = [centro]
+                    list_bril = brilho
+                    #print("brilho -> ", brilho)
+                    # uma lista guarda as posicoes da sensacoes que o wumpus deve ler
+                    print("\n\n")
+
+                elif (A[i][j] == 'H'):  # quando o programa achar o buraco (Hole)
+                    print(f"buraco em l[{i}] c[{j}]\n", end="")
+                    norte = (i+1, j) if i < (l-1) else None
+                    sul = (i-1, j) if i > 0 else None
+                    leste = (i, j+1) if j < (c-1) else None
+                    oeste = (i, j-1) if j > 0 else None
+                    brisa = [norte, sul, leste, oeste]
+                    list_brisa += [brisa]
+                    #print("brisa -> ", brisa)
+                    print("\n\n")
+
+                elif (A[i][j] == 'W'):  # quando o programa achar o wumpus
+                    print(f"wumpus em l[{i}] c[{j}]\n", end="")
+                    norte = (i+1, j) if i < (l-1) else None
+                    sul = (i-1, j) if i > 0 else None
+                    leste = (i, j+1) if j < (c-1) else None
+                    oeste = (i, j-1) if j > 0 else None
+                    fedor = [norte, sul, leste, oeste]
+                    list_fedor += [fedor]
+                    #print("fedor -> ", fedor)
+                    print("\n\n")
+                    
+                elif (A[i][j] == 'A'):  # quando o programa achar o agente
+                    print(f"\nagente em l[{i}] c[{j}]\n", end="")
+                    print('\n\n')
+
+        list_pos = [list_bril, list_brisa, list_fedor]
+    
+    def sens_pos(self, pos_ag, tam): #Função para identificar se o Ag está sentindo Brisa, Fedor ou Brilho
+        global list_pos
+        sen = []
+        brisa = []
+        fedor = []
+        brilho = []
+
+        for i in range(1):
+            #print('Casa: ',list_pos[i])
+            brilho += list_pos[i]
+            for i in range(len(brilho)):
+                if (brilho[i] == pos_ag):
+                    sen += 'Br'
+
+        for i in range(1, len(list_pos)-1, 1):
+            for j in range(tam-1):
+                brisa += list_pos[i][j]
+                for k in range(len(brisa)):
+                    #print('Casa: ', brisa[k])
+                    if (brisa[k] == pos_ag):
+                        sen += 'B'
+                    if(sen == 'B'): break
+
+        for i in range(len(list_pos)-1, len(list_pos), 1):
+            for j in range(1):
+                fedor += list_pos[i][j]
+                for k in range(len(fedor)):
+                    #print('Casa: ', fedor[k])
+                    if (fedor[k] == pos_ag):
+                        sen += 'F'
+        return sen
